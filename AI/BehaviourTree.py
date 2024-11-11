126762844d6ce022bcca59b9dcfadacd4705b1b7
Tomasz Potoczko <tomasz.spezik@gmail.com>
2024-11-11 15:23:33 +0100

    Finished behaviour trees
    Signed-off-by: Tomasz Potoczko <tomasz.spezik@gmail.com>

--- AI/BehaviourTree.py
+++ AI/BehaviourTree.py
@@ -19,9 +19,13 @@ class ControlFlowNode(BehaviourNode):
         super().__init__(name, parent)
         self.condition = condition
         self.children = children
+
     
     def activate(self, *args):
         return self.condition(*args)
+    
+    def get_child_by_index(self, idx: int):
+        return self.children[idx]
     
     def print_children(self):
         print(f'Children of {self}')
@@ -41,18 +45,31 @@ class ExecutionNode(BehaviourNode):
 class BehaviourTree:
     def __init__(self, mob, tree: Dict[Callable, List[Callable, ]]):
         self.mob = mob
-        self.root = self.constructTree(tree)
-        self.print_tree()
-        sys.exit()
+        self.root = self.construct_tree(tree)
+        # self.print_tree()
+        # sys.exit()
 
-    def find_action(self) -> Callable:
+    def find_action(self, current_node=None) -> Callable:
         '''
         We search the tree and return the method (action)
         which will be ran by the mob
         '''
+        if current_node == None:
+            current_node = self.root
+
+        if isinstance(current_node, ControlFlowNode):
+            # print('ControlFlowNode:', current_node)
+            condition_result = current_node.activate()
+            print(current_node, '->', condition_result)
+            if condition_result:
+                return self.find_action(current_node.get_child_by_index(1))
+            return self.find_action(current_node.get_child_by_index(0))
+        
+        return current_node.executionBehaviour
+    
         return lambda: print('Finding actions not implemented yet!')
 
-    def constructTree(self, tree):
+    def construct_tree(self, tree):
         # print('\nCONSTRUCTING BEHAVIOUR TREE!!!')
         def get_children_nodes(children: List[Callable, ], recursionLevel=0) -> List[BehaviourNode]:
             # print('\n', '\t'*recursionLevel, 'get_children_nodes level:', recursionLevel)
@@ -101,3 +118,5 @@ class BehaviourTree:
             print('\t'*(recursion_level+1), '\x1B[3mon False:\x1B[23m' if idx%2==0 else '\x1B[3mon True:\x1B[23m',child)
             if isinstance(child, ControlFlowNode):
                 self.print_tree(child, recursion_level+1)
+
+                
